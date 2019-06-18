946f1a82dfe4119b3b7e1348d0b58b1c3812be4f
Jaachimma Onyenze <onyenze.jaachimma@gmail.com>
2019-06-18 06:57:35 +0100

    feat(): create form for adding new property
    - create modal in properties.html page
    - add form to modal for adding new properties
    - add form.css file for form styling
    - implement modal function using javascript
    
    [Delivers #166628622]

--- UI/javascript/properties.js
+++ UI/javascript/properties.js
@@ -1,6 +1,35 @@
+/* menu bar */
 const menuButton = document.querySelector('.menu');
 const menuBar = document.querySelector('.menu-drop-down');
 
 menuButton.addEventListener('click', (event) => {
   menuBar.classList.toggle('on');
 });
+
+/* modal */
+const modal = document.getElementById('myModal');
+const modalCloseBtn = document.querySelectorAll('.modal-close-btn');
+const allForms = document.querySelectorAll('.property-form');
+const addPropertyForm = document.querySelector('.add-property-form');
+const addBtn = document.querySelector('.add-property-btn');
+
+modalCloseBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    allForms.forEach((form) => {
+      form.style.display = 'none';
+    });
+  });
+});
+
+addBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+  addPropertyForm.style.display = 'block';
+});
+
+/* loader */
+const formBtn = document.querySelector('.form-btn');
+
+formBtn.addEventListener('click', () => {
+  document.querySelector('.loader').style.display = 'block';
+})
